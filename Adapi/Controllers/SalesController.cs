4e228e8209d8cd88f61799ea8d550f13814a63a7
Marc Fuchshofer <marc.fuchshofer@gmail.com>
2020-02-16 14:53:06 +0100

    Add project files.

---
+++ Adapi/Controllers/SalesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Adapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesController : ControllerBase
+    {
+        private readonly ILogger<SalesController> _logger;
+
+        public SalesController(ILogger<SalesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
